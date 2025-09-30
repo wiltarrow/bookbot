afeb692580544e153a8fbab5a0cda17073aec398
Wiltarrow <leohomestuck@gmail.com>
2025-09-30 10:00:08 -0400

    main now counts the words in the file

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ def get_book_text(file):
         contents = f.read()
     return contents
 
+def number_of_words(text):
+    count = 0
+    for words in text.split():
+        count += 1
+    return count
+
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
+    text = get_book_text("./books/frankenstein.txt")
+    num_words = number_of_words(text)
+    print(f"Found {num_words} total words")
 
 main()
