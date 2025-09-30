7d06efd103a06adea74b1868bcc5240442328d3f
Wiltarrow <leohomestuck@gmail.com>
2025-09-30 09:54:58 -0400

    main.py regurgitates the file's text

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(file):
+    with open(file) as f:
+        contents = f.read()
+    return contents
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+
+main()
